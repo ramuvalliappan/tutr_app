8f6a9bb67565954a1d8eada7eaa100a291a30fbf
lucaszcai <lucas.z.cai@gmail.com>
2020-07-26 19:31:40 -0500

    some fixes to login screen

--- lib/screens/Sign_in_page.dart
+++ lib/screens/Sign_in_page.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 
-class BadgeScreen extends StatefulWidget {
+class SignInScreen extends StatefulWidget {
   @override
-  _BadgeScreenState createState() => _BadgeScreenState();
+  _SignInScreenState createState() => _SignInScreenState();
 }
 
-class _BadgeScreenState extends State<BadgeScreen> {
+class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 100,),
-            Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
+      body: SingleChildScrollView(
+        child: Container(
+            child: Column(mainAxisAlignment: MainAxisAlignment.start, children: <
+                Widget>[
+          SizedBox(
+            height: 100,
+          ),
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    Text("Welcome",
-                      style: TextStyle(color: Colors.black, fontSize: 40),),
-                    Text("Sign In",
-                      style: TextStyle(color: Colors.black, fontSize: 60),),
+                    Text(
+                      "Welcome",
+                      style: TextStyle(color: Colors.black, fontSize: 40),
+                    ),
+                    Text(
+                      "Sign In",
+                      style: TextStyle(color: Colors.black, fontSize: 60),
+                    ),
                     Padding(
                         padding: EdgeInsets.all(20),
                         child: Column(
@@ -33,74 +39,88 @@ class _BadgeScreenState extends State<BadgeScreen> {
                                 decoration: BoxDecoration(
                                     color: Colors.white,
                                     borderRadius: BorderRadius.circular(10),
-                                    boxShadow: [BoxShadow(
-                                        color: Colors.black,
-                                        blurRadius: 20,
-                                        offset: Offset(0, 10)
-                                    )
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Colors.black.withOpacity(0.2),
+                                          blurRadius: 7,
+                                          offset: Offset(0, 5))
                                     ]),
                                 child: Column(
                                   children: <Widget>[
                                     Container(
                                       decoration: BoxDecoration(
-                                        border: Border(bottom: BorderSide(color: Colors.black))
-                                      ),
+                                          border: Border(
+                                        bottom: BorderSide(color: Colors.black),
+                                      )),
                                       child: TextField(
-                                        decoration: InputDecoration(
-                                          hintText: "Email Address",
-                                          hintStyle: TextStyle(color: Colors.grey),
-                                          border: InputBorder.none
-                                        )
-                                      ),
+                                          decoration: InputDecoration(
+                                              hintText: "Email Address",
+                                              hintStyle:
+                                                  TextStyle(color: Colors.grey),
+                                              border: InputBorder.none)),
                                     ),
-                                    SizedBox(height: 40,),
+                                    SizedBox(
+                                      height: 40,
+                                    ),
                                     Container(
                                       decoration: BoxDecoration(
-                                          border: Border(bottom: BorderSide(color: Colors.black))
-                                      ),
+                                          border: Border(
+                                              bottom: BorderSide(
+                                                  color: Colors.black))),
                                       child: TextField(
                                           decoration: InputDecoration(
                                               hintText: "Password",
-                                              hintStyle: TextStyle(color: Colors.grey),
-                                              border: InputBorder.none
-                                          )
-                                      ),
+                                              hintStyle:
+                                                  TextStyle(color: Colors.grey),
+                                              border: InputBorder.none)),
                                     ),
                                   ],
-                                )
+                                )),
+                            SizedBox(
+                              height: 40,
                             ),
-                            SizedBox(height: 40,),
-                            Text("Forgot Password?", style: TextStyle(color: Colors.grey),),
-                            SizedBox(height: 40,),
+                            Text(
+                              "Forgot Password?",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            SizedBox(
+                              height: 40,
+                            ),
+                            Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.green[900]),
+                                //color: Color(0xff42f572)),
+                                child: Center(
+                                    child: Text(
+                                  "Sign In",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black),
+                                ))),
+                            SizedBox(
+                              height: 40,
+                            ),
                             Container(
                               height: 50,
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                    color: Colors.green[900]
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.lightGreen),
+                              child: Center(
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ),
-                                child: Center(
-                                child: Text("Sign In", style: TextStyle(fontWeight: FontWeight.bold),)
-                        )
-                    ),
-                    SizedBox(height: 40,),
-                            Row(
-                              children: <Widget>[
-                                Container(
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(50),
-                                      color: Colors.lightGreen
-                                  )
-                                )
-                                    ]
-                                )
-                              ],
                             )
-                    ),
-                ]
-            )
-    ),
-    ]
-    )
+                          ],
+                        )),
+                  ])),
+        ])),
       ),
     );
+  }
+}
