82c940beebc7c0b3831035b365bdbd39de3f3e04
overdrive88 <68761474+overdrive88@users.noreply.github.com>
2020-07-28 17:20:43 -0700

    Add files via upload

---
+++ tutr_app-master/lib/screens/badges_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BadgesScreen extends StatefulWidget {
+  @override
+  _BadgesScreenState createState() => _BadgesScreenState();
+}
+
+class _BadgesScreenState extends State<BadgesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
