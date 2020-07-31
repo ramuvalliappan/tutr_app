94b5631acbe969760d898ec59813b592b2d25a5a
ramuvalliappan <ramu.m.valliappan@gmail.com>
2020-07-31 19:04:34 -0400

    finished home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,40 +13,52 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List names = ['Peter', 'John', 'David', 'Mary'];
   List subjects = ['Math', 'Calculus', 'History', 'English'];
-  List profiles = ['assets/profile_picture.webp', 'assets/john.jpg', 'assets/david.jpg', 'assets/mary.jpg'];
+  List profiles = [
+    'assets/profile_picture.webp',
+    'assets/john.jpg',
+    'assets/david.jpg',
+    'assets/mary.jpg'
+  ];
+  List assignments = [
+    'Read two chapters of Calculus BC',
+    'Answer the first five questions given about Lord of the Flies',
+    'Finish the essay about WWII',
+    'Answer math questions 1-10'
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 25.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0),
-            child: Text(
-              'Your Tutors',
-              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w400),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 25.0,
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Container(
-            height: 250.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Text(
+                'Your Tutors',
+                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w400),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              height: 250.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
                 itemCount: names.length, itemBuilder: (context, index) {
-                  return Padding(
+                return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Container(
                       height: 250.0,
                       width: 250.0,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.blue
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.blue
                       ),
                       child: Stack(
                         children: [
@@ -55,20 +67,72 @@ class _HomeScreenState extends State<HomeScreen> {
                             child: Container(
                               height: 250.0,
                               width: 250.0,
-                              child: Image.asset(profiles[index], fit: BoxFit.cover,),
+                              child: Image.asset(profiles[index], fit: BoxFit
+                                  .cover,),
                             ),
                           ),
 
-                          Text(names[index]),
+
+                          Text(
+                              names[index],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0,
+                                color: Colors.white,
+                              ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            subjects[index],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                              color: Colors.white,
+                            ),
+                          ),
                         ],
                       ),
-                    ),
-                  );
-                }
+                    )
+                );
+              },
+              ),
             ),
-          ),
-        ],
-      ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Text(
+                'Your Assignments',
+                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w400),
+              ),
+            ),
+              Container(
+              height: 250.0,
+    child: ListView.builder(
+    scrollDirection: Axis.vertical,
+    itemCount: assignments.length, itemBuilder: (context, index) {
+    return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+    height: 30.0,
+    width: 300.0,
+    decoration: BoxDecoration(
+      border: Border(
+        bottom: BorderSide(
+          color: Colors.black)),
+    borderRadius: BorderRadius.circular(20.0),
+    color: Colors.blue
+    )
+    ),
     );
-  }
+    }
+    )
+    )
+    ]
+    )
+    );
+}
 }
